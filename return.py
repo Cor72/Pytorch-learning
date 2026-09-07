30c601e0bb23df5839cf17e418433812aec7eb80
Cor72 <Qiguo72@outlook.com>
2026-09-07 23:57:24 +0800

    ruiotgyu

--- return.py
+++ return.py
@@ -4,12 +4,25 @@ from torch.utils.data import TensorDataset
 from torch import nn
 from torch import optim
 from sklearn.datasets import make_regression
-import matpolotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 plt.rcParams['font.sans-serif'] = ['SimHei']
 plt.rcParams['axes.unicode_minus'] = False
 
+
 def create_dataset():
-    x,y,coef = make_regression(
-        n_samples=100, n_features=1, n_informative=1, noise=10
+    x, y, coef = make_regression(
+        n_samples=100,
+        n_features=1,
+        n_informative=1,
+        noise=10,
+        coef=True,
+        random_state=3
     )
+    print(coef)
+    print(type(x))
+    return x, y, coef
+
+
+if __name__ == '__main__':
+    create_dataset()
